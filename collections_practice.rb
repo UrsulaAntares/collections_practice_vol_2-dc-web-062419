f65543304fe35356afe9a1177e653af40d68ffcb
Ursula Miller <ursula.miller@gmail.com>
2019-06-21 02:39:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,72 @@
 # your code goes here
+require 'pry'
+
+def begins_with_r(array)
+ i = 0  
+ while i < array.length
+    if array[i].start_with?("r")
+      i+=1
+      true
+    else
+      return false
+    end
+ end
+ true
+end
+
+
+def contain_a(array)
+  i=0
+  the_a_team = []
+  while i < array.length
+    if  array[i].include?("a")
+      the_a_team << array[i]
+    end
+    i+=1
+  end
+  the_a_team
+end
+
+def first_wa(array)
+  i=0
+  wa_wa_trombone =""
+  while i < array.length
+    if array[i].to_s.start_with?("wa")
+      wa_wa_trombone = array[i]
+      break if wa_wa_trombone != ""
+    end
+  i+=1  
+  end
+  wa_wa_trombone
+end
+
+
+def remove_non_strings(array)
+ i=0
+ remains_of_the_array = []
+ while i < array.length
+# binding.pry
+  if array[i].class == String
+    remains_of_the_array << array[i]
+  end
+  i+=1
+ end
+ remains_of_the_array
+end
+
+def count_elements(array)
+  array
+  do_array_me = []
+  array.each do | hash |
+    new_hash = {}
+    hash.each do | key, value |
+      binding.pry
+      new_hash[value] = 1
+      do_array_me << new_hash
+#      binding.pry
+    end
+  end
+# I need to return an array of hashes, in each hash anotehr level of hashes; the first sub-hash in each hash is the count of the instances of the second sub-hash's value, 
+  do_array_me
+#  binding.pry
+end
